11cb4062ee2366e4e25570af94dc5e8805034459
Kenny Pflug <kenny.pflug@live.de>
2024-03-04 08:08:33 +0100

    feat: add Mediator
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- WebApp/CompositionRoot/DependencyInjection.cs
+++ WebApp/CompositionRoot/DependencyInjection.cs
@@ -21,6 +21,7 @@ public static class DependencyInjection
            .AddCommonValidation()
            .AddToDoModule()
            .AddContactsModule()
+           .AddMediator(options => options.ServiceLifetime = ServiceLifetime.Transient)
            .AddHealthChecks();
         return builder;
     }
--- WebApp/ToDo/GetToDos/GetToDosEndpoint.cs
+++ WebApp/ToDo/GetToDos/GetToDosEndpoint.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
+using Mediator;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 
@@ -13,13 +15,31 @@ public static class GetToDosEndpoint
         return app;
     }
 
-    public static async Task<IResult> GetToDos(
-        IGetToDosSession session,
-        CancellationToken cancellationToken = default
-    )
+    public static async Task<IResult> GetToDos(ISender sender, CancellationToken cancellationToken = default)
     {
-        var toDoList = await session.GetToDoListAsync(cancellationToken);
-        var dtoList = toDoList.MapToDtoList();
+        var dtoList = await sender.Send(ToDoRequest.Instance, cancellationToken);
         return Results.Ok(dtoList);
+    }
+}
+
+public sealed class ToDoRequest : IRequest<List<ToDoListDto>>
+{
+    public static ToDoRequest Instance { get; } = new();
+}
+
+public sealed class ToDoRequestHandler : IRequestHandler<ToDoRequest, List<ToDoListDto>>
+{
+    private readonly IGetToDosSession _session;
+
+    public ToDoRequestHandler(IGetToDosSession session)
+    {
+        _session = session;
+    }
+    
+    public async ValueTask<List<ToDoListDto>> Handle(ToDoRequest request, CancellationToken cancellationToken)
+    {
+        var toDoList = await _session.GetToDoListAsync(cancellationToken);
+        var dtoList = toDoList.MapToDtoList();
+        return dtoList;
     }
 }
